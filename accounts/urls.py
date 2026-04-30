1f70a49ff7138ddc64772cecf3d51f33ca539b90
ghadi <ghadizarb@gmail.com>
2026-04-30 22:09:28 +0300

    add seeker signup, login, and logout. add the templates and created the html files

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/seeker/', views.signup_seeker, name='signup_seeker'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
