77436749653b7589b909e69572998c640c2c5588
Rimas Alshahrani <remasmushbab@gmail.com>
2026-04-30 17:58:18 +0300

    Fix migration conflicts and reset helper models

--- helpers/models.py
+++ helpers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100, verbose_name="اسم المدينة")
+
+    class Meta:
+        verbose_name = "مدينة"
+        verbose_name_plural = "المدن"
+
+    def __str__(self):
+        return self.name
--- sanad/settings.py
+++ sanad/settings.py
@@ -125,3 +125,8 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
