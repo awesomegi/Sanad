415e93276cbf7a29c014de574e85bae63a3231c6
ghadi <ghadizarb@gmail.com>
2026-05-02 02:37:33 +0300

    added variable placeholder

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#booking.service.name (string)
+#booking.scheduled_date (date)
+#booking.scheduled_start_time (time)
+#booking.hours (decimal)
+#booking.total_amount (decimal)
+#booking.helper.user.first_name
